5bc6152378a8734b7370fb946f03f1497274edba
Hikayat Journal <muhammad.titian@gmail.com>
2026-07-20 13:44:52 +0700

    Add files via upload

--- app/api/socmed/statistik/route.ts
+++ app/api/socmed/statistik/route.ts
@@ -1,1 +1,12 @@
+import { createSheetRoute } from "@/lib/createSheetRoute";
 
+export const { GET, POST } = createSheetRoute("SHEET_ID_SOCMED_STATISTIK", [
+  "id_statistik",
+  "id_konten",
+  "minggu_ke",
+  "tanggal_input",
+  "views",
+  "likes",
+  "reach",
+  "engagement_rate",
+]);
