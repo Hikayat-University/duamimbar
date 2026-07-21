562c38c486ceeaf1e8492d9284bd9e7cdb3e994f
Hikayat Journal <muhammad.titian@gmail.com>
2026-07-21 19:01:57 +0700

    Delete app/api/socmed/kanal directory

--- app/api/socmed/kanal/route.ts
+++
@@ -1,9 +0,0 @@
-import { createSheetRoute } from "@/lib/createSheetRoute";
-
-export const { GET, POST } = createSheetRoute("SHEET_ID_SOCMED_KANAL", [
-  "id_kanal",
-  "nama_kanal",
-  "platform",
-  "link",
-  "dibuat_pada",
-]);
