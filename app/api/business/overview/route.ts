b3f029996d2fae0ac699c6a247f366aa0803c743
Hikayat Journal <muhammad.titian@gmail.com>
2026-07-21 17:13:05 +0700

    Delete app/api/business/overview/route.ts

--- app/api/business/overview/route.ts
+++
@@ -1,9 +0,0 @@
-import { createSheetRoute } from "@/lib/createSheetRoute";
-
-export const { GET, POST } = createSheetRoute("SHEET_ID_BUSINESS_OVERVIEW", [
-  "id_bisnis",
-  "nama_bisnis",
-  "deskripsi_singkat",
-  "bmc_link",
-  "status",
-]);
